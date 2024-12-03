fc0d02e1e55f3f81583c9eeb89474d4f96325f01
RoHaN <mdrohanislam444@gmail.com>
2024-12-03 19:54:46 +0600

    Add files via upload

---
+++ A_Maximum_GCD.cpp
@@ -0,0 +1,26 @@
+// Date: 03-12-2024 at 19:52 BST
+// Link: https://codeforces.com/problemset/problem/1370/A
+#include <bits/stdc++.h>
+#define null nullptr
+#define ll long long int
+#define nl '\n'
+using namespace std;
+void solve()
+{
+    int n;
+    cin >> n;
+    cout << n / 2 << nl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int test;
+    cin >> test;
+    while (test--)
+    {
+        solve();
+    }
+    return 0;
+}
